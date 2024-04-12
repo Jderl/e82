8f97cc9604ee641567e2deb87187c81688a8bd84
Julius Derla <derlajulius48@gmail.com>
2024-04-12 18:11:38 +0800

    Insallation of Passport and Creation Api Routes, Login, Register, ForgotPassword, ResetPassword

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,31 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotController;
+use App\Http\Controllers\ResetController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+*/
+
+
+
+//login route 
+Route::post('/login',[AuthController::class,'login']);
+//registration route 
+Route::post('/login',[AuthController::class,'Register']);
+//Forgot route 
+Route::post('/login',[ForgotController::class,'ForgotPassoword']);
+//Reset password route 
+Route::post('/login',[ResetController::class,'ResetPassword']);
+
+
+//get user route
+//user middleware to check if the user is logged
+Route::get('/user',[UserController::class,'User'])->middleware('auth:api');
